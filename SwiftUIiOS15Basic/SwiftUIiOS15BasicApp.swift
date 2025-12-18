27d3257daa2d84ca2200600419f45bd4152c13e0
wannabemountainking <kdy0096@gmail.com>
2025-12-18 18:29:32 +0900

    Initial Commit

---
+++ SwiftUIiOS15Basic/SwiftUIiOS15BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIiOS15BasicApp.swift
+//  SwiftUIiOS15Basic
+//
+//  Created by YoonieMac on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIiOS15BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
